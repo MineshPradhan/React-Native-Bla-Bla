c9b194855d9256ddbb2f5fea34d3aed795e82fec
MineshPradhan <mineshpradhan2002@gmail.com>
2022-08-25 23:48:54 +0530

    button added

---
+++ components/StyledButton/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        width: '100%',
+        padding: 10,
+    },
+    button:{
+        backgroundColor: 'white',
+        height: 40,
+        justifyContent: 'center',
+    },
+    text:{
+        
+    }
+});
+
+export default styles;
